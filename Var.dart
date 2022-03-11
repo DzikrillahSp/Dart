2e64898322fc3eae98fda2fd8446edd2ec375bc7
dzikrillahsukma <80307090+DzikrillahSp@users.noreply.github.com>
2022-03-11 15:40:09 +0700

    Update and rename var.dart to Var.dart

---
+++ Var.dart
@@ -0,0 +1,8 @@
+void main (){
+  var name = "Dzikrillah" ; // Type 
+  var angka = 5;
+  var TodayIsFriday = false ;
+  print (name); // "Dzikrillah"
+  print(angka); // 5
+  print(TodayIsFriday); // false
+}
--- var.dart
+++
@@ -1,8 +0,0 @@
-void main (){
-  var name = "Chuuya" ; // Type 
-  var num = 29;
-  var Fyoyacanon = false ;
-  print (name); // "Chuuya"
-  print(num); // 20
-  print(Fyoyacanon); // false
-}
