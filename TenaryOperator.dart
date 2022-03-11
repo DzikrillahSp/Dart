a994817373aa6480bebe876402868654e94bb874
dzikrillahsukma <80307090+DzikrillahSp@users.noreply.github.com>
2022-03-12 04:04:06 +0700

    Create TenaryOperator.dart

---
+++ TenaryOperator.dart
@@ -0,0 +1,6 @@
+void main() {
+bool suka = true;
+  print('Apakah Anda ingin install aplikasi dart?');
+  String jawaban = suka ? "Anda akan menginstall aplikasi dart" : "Aborted";
+  print("Y/T : $jawaban");
+}
