74ce32bcc14b45d0d0ecc9127188d70b9ccc2ef7
dzikrillahsukma <80307090+DzikrillahSp@users.noreply.github.com>
2022-03-11 23:08:12 +0700

    Update ifConditional(Premis).dart

--- ifConditional(Premis).dart
+++ ifConditional(Premis).dart
@@ -1,6 +1,10 @@
 void main () {
   var mood = "Sad"; 
+  var suasana = "Happy";
   if (mood == "Sad") {
     print ("Sedih Sekali");
+  }
+  if (suasana == "Happy"){
+    print("Senang sekali");
   }
 }
